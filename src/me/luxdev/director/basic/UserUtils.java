236d548fa5db9197f9878112c73fd99c5ed07333
ElDzi <jaroaga@onet.pl>
2016-05-18 12:48:55 +0200

    Update UserUtils.java

--- src/me/luxdev/director/basic/UserUtils.java
+++ src/me/luxdev/director/basic/UserUtils.java
@@ -7,5 +7,14 @@ import java.util.List;
 public class UserUtils {
 	
 	public static List<User> users = new ArrayList<User>(); 
+	
+		public static User getUser(String name) throws Exception{
+		for (User u : users) {
+		   if (u.getName().equals(name)) {
+				return u;
+		   }
+		}
+		return throw new Exception("User is null");
+	}
 
 }
